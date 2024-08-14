2a47bf5de8d8a249d9dff88af7c5ff308b6fe48c
AB HEMANTH VARMA <130243444+hemanthvarma-ab@users.noreply.github.com>
2024-08-15 01:03:05 +0530

    Create patterns.java

---
+++ patterns.java
@@ -0,0 +1,26 @@
+1./* Inverted & Rotated Half Pyramid 
+   *
+  **
+ ***
+****
+ 
+ */
+
+import java.util.Scannner;
+
+ class hello{
+    public static void main(String args []){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for( int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+ }

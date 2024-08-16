d9342e51ca398fd7b59912e6c49f0c4ef836acad
AB HEMANTH VARMA <130243444+hemanthvarma-ab@users.noreply.github.com>
2024-08-16 23:59:33 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -158,3 +158,30 @@ import java.util.Scanner;
         }
     }
  }
+
+
+/* 7. solid rhombus
+    *****
+   *****
+  *****
+ *****
+*****
+ */
+
+import java.util.Scanner;
+
+ class hello{
+    public static void main(String args []){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=n;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+ }

e98c5c4fbe5085c70e726c4f7460ce63e9cbb931
AB HEMANTH VARMA <130243444+hemanthvarma-ab@users.noreply.github.com>
2024-08-16 21:49:05 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -102,3 +102,29 @@ import java.util.Scanner;
         }
     }
  }
+
+
+/*  5. Floyd's Triangle
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+ */
+
+import java.util.Scanner;
+
+ class hello{
+    public static void main(String args []){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+ }

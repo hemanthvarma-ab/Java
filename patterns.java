d23093a092edf73e34752d9978f148f1997af7a0
AB HEMANTH VARMA <130243444+hemanthvarma-ab@users.noreply.github.com>
2024-08-16 21:57:05 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -128,3 +128,33 @@ import java.util.Scanner;
         }
     }
  }
+
+/*  6. 0-1 Triangle
+1
+0 1
+0 1 0
+1 0 1 0
+1 0 1 0 1
+ */
+
+import java.util.Scanner;
+
+ class hello{
+    public static void main(String args []){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+               if(num%2==0){
+                System.out.print(0+" ");
+               }
+               else{
+                System.out.print(1+" ");
+               }
+               num++;
+            }
+            System.out.println();
+        }
+    }
+ }

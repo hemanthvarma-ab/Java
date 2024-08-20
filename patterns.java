f69cb7d074aa2064d758f50fc2c0ec629e6025b0
AB HEMANTH VARMA <130243444+hemanthvarma-ab@users.noreply.github.com>
2024-08-20 22:55:07 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -256,3 +256,31 @@ import java.util.Scanner;
         }
     }
  }
+
+/* 10. Palindrome Pyramid
+    1
+   212
+  32123
+ 4321234
+543212345
+
+ */
+import java.util.Scanner;
+ class hello{
+    public static void main(String args []){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int k=i;k>=1;k--){
+                System.out.print(k);
+            }
+            for(int k=2;k<=i;k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+ }

68fcd13b7d16e0d3ec551102e30225896e10965d
AB HEMANTH VARMA <130243444+hemanthvarma-ab@users.noreply.github.com>
2024-09-03 23:08:43 +0530

    Update Exercise.java

--- Exercise.java
+++ Exercise.java
@@ -32,3 +32,46 @@ import java.util.Scanner;
         System.out.println(oddsum(n));
     }
  }
+
+// 3. Write a program to enter the numbers till the user wants and at the end it should display the count of positive, negative and zeros entered. 
+
+import java.util.Scanner;
+public class hello {
+    public static void counts(int n){
+        int pos_count=0;
+        int neg_count=0;
+        int zero_count=0;
+        for(int i=1;i<=n;i++){
+            Scanner sc=new Scanner(System.in);
+            int n1=sc.nextInt();
+            if(n1>0) pos_count++;
+            else if(n1<0) neg_count++;
+            else zero_count++;
+        }
+        System.out.println("Number of positive numbers = "+pos_count);
+        System.out.println("Number of negative numbers = "+neg_count);
+        System.out.println("Number of zeros = "+zero_count);
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of numbers you want : ");
+        int n=sc.nextInt();
+        counts(n);
+    }
+}
+
+/* 4.Write a function that takes in the radius as input and returns the circumference of a circle.  */
+
+import java.util.Scanner;
+public class hello {
+    public static double circumference(int r){
+        double circum=2*Math.PI*r;
+        return circum;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the radius of the circle : ");
+        int r=sc.nextInt();
+        System.out.println(circumference(r));
+    }
+}

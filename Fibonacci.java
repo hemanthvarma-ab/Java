e57208851873516bd3d5ca6f11cf34da9289f7c6
AB HEMANTH VARMA <130243444+hemanthvarma-ab@users.noreply.github.com>
2024-09-04 23:59:18 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,7 @@
 // Write a program to print Fibonacci series of n terms where n is input by user : 
 // 0 1 1 2 3 5 8 13 21 ..... 
 // In the Fibonacci series, a number is the sum of the previous 2 numbers that came before it.
+// Without Using Recursion
 
 import java.util.*;
 public class hello {
@@ -20,5 +21,24 @@ public class hello {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         fibonacci(a);
+    }
+}
+
+// Using Recursion
+
+import java.util.*;
+public class hello {
+    public static int fibonacci(int n){
+        if(n<=1){
+            return n;
+        }
+        return fibonacci(n-1)+fibonacci(n-2);
+        
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=0;i<a;i++)
+        System.out.print(fibonacci(i)+" ");
     }
 }

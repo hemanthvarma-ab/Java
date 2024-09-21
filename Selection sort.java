3ee8c943b67d5e4a1d0e374bd3943b6bf54135b8
AB HEMANTH VARMA <130243444+hemanthvarma-ab@users.noreply.github.com>
2024-09-22 00:18:43 +0530

    Create Selection sort.java

---
+++ Selection sort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class hello{
+    static void selectionsort(int n,int [] arr){
+        for(int i=0;i<n-2;i++){
+            int min=i;
+            for(int j=i;j<n-1;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        selectionsort(n, arr);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}

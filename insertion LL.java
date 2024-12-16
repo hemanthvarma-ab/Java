acd0fb96be328dab30ac59eeb3a7e1ffdffa4425
AB HEMANTH VARMA <130243444+hemanthvarma-ab@users.noreply.github.com>
2024-12-17 00:24:05 +0530

    Update insertion LL.java

--- insertion LL.java
+++ insertion LL.java
@@ -1,3 +1,5 @@
+// Inserting a node at end    (node.next=tail , tail=node)   (uses tail)
+
 import java.util.*;
 
 class Node{
@@ -19,7 +21,7 @@ class LL{
             head=nn;
             tail=nn;
         }
-        else{                    //next points to new node
+        else{                    //next points to new node  (insertion at end)
             tail.next=nn;        
             tail=nn;
         }
@@ -50,3 +52,67 @@ class Practice{
         l.display();
     }
 }
+
+
+// Inserting a node at  front or begining   (node.next=head   , head=node)       (uses head)
+
+
+import java.util.*;
+
+class Node{                   
+    int data;
+    Node next;
+    Node(int data){                           //create new node
+        this.data=data;
+        this.next=null;
+    }
+}
+
+class LL{
+    Node head=null;
+    Node tail=null;
+    public void add(int data){
+        Node nn=new Node(data);
+        if(head==null){
+            head=nn;
+            tail=nn;
+        }
+        else{
+            nn.next=head;                  //next points to new node  (insertion at front)
+            head=nn;
+        }
+    }
+
+    public void display(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+}
+
+class Practice{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        LL l=new LL();
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            l.add(arr[i]);
+        }
+        l.display();
+    }
+}
+
+
+
+
+
+
+
+

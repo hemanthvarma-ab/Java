f54ab5c143d8894a482fc423ebc7e828cc20c99d
AB HEMANTH VARMA <130243444+hemanthvarma-ab@users.noreply.github.com>
2024-12-18 22:03:01 +0530

    Update insertion LL.java

--- insertion LL.java
+++ insertion LL.java
@@ -1,88 +1,41 @@
-// Inserting a node at end    (node.next=tail , tail=node)   (uses tail)
-
 import java.util.*;
 
-class Node{
+class Node{                     // creation of Node
     int data;
     Node next;
-    Node(int data){           //create new node
+    Node(int data){
         this.data=data;
         this.next=null;
     }
 }
 
-class LL{
+class LinkedList{
     Node head=null;
-    Node tail=null;
+    Node temp=null;
 
-    public void add(int data){
+    public void addFirst(int data){          // Insertion at beginning 
         Node nn=new Node(data);
         if(head==null){
             head=nn;
-            tail=nn;
-        }
-        else{                    //next points to new node  (insertion at end)
-            tail.next=nn;        
-            tail=nn;
-        }
-    }
-
-    public void display(){
-        Node temp=head;          // start from head
-        while(temp!=null){
-            System.out.print(temp.data+"->");
-            temp=temp.next;
-        }
-        System.out.println("null");
-    }
-}
-
-class Practice{
-    public static void main(String [] args){
-        Scanner sc=new Scanner(System.in);
-        LL l=new LL();
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        for(int i=0;i<n;i++){
-            l.add(arr[i]);
-        }
-        l.display();
-    }
-}
-
-
-// Inserting a node at  front or begining   (node.next=head   , head=node)       (uses head)
-
-
-import java.util.*;
-
-class Node{                   
-    int data;
-    Node next;
-    Node(int data){                           //create new node
-        this.data=data;
-        this.next=null;
-    }
-}
-
-class LL{
-    Node head=null;
-    Node tail=null;
-    public void add(int data){
-        Node nn=new Node(data);
-        if(head==null){
-            head=nn;
-            tail=nn;
+            temp=nn;
         }
         else{
-            nn.next=head;                  //next points to new node  (insertion at front)
+            nn.next=head;
             head=nn;
         }
     }
 
+    public void addLast(int data){         // Insertion at end
+        Node nn=new Node(data);
+        if(head==null){
+            head=nn;
+        }
+        else{
+            temp.next=nn;
+            temp=nn;
+        }
+    }
+    
     public void display(){
         Node temp=head;
         while(temp!=null){
@@ -91,28 +44,26 @@ class LL{
         }
         System.out.println("null");
     }
+
 }
 
 class Practice{
     public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
-        LL l=new LL();
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-        for(int i=0;i<n;i++){
-            l.add(arr[i]);
-        }
-        l.display();
+        LinkedList ll=new LinkedList();
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.display();
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.display();
     }
 }
 
 
-
-
-
-
-
-
+/*
+      Output :
+      1->2->3->null
+      1->2->3->4->5->null
+*/
